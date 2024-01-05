ef666ae3ef44c2794ebba17681b530ba5138dc81
Vishal7410 <vishalpardhi2001@gmail.com>
2024-01-05 23:06:32 +0530

    Cheap_Me_Code_Running

---
+++ 5_Cheap_me_code/src/Component/JustDo.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function JustDo() {
+  return (
+    <div>JustDo</div>
+  )
+}
+
+export default JustDo
+
+// const heading = React.createElement("h1",
+// {
+//  id: "title",
+//  key: "h1",  
+// },
+// "Heading 1 for parcel"
+// );
+// console.log(heading);
