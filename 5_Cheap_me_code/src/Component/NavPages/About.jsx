8f045ce9b923ec3afcd8381fb90df685672ee1a8
Vishal7410 <vishalpardhi2001@gmail.com>
2024-02-09 18:36:35 +0530

    componentLifeCyledDone

--- 5_Cheap_me_code/src/Component/NavPages/About.jsx
+++ 5_Cheap_me_code/src/Component/NavPages/About.jsx
@@ -1,44 +1,43 @@
-import React from "react"
+import React from "react";
 import Profile from "./Profile";
 import ProfileClassComponent from "./ProfileClass";
-class About extends React.Component{
- 
-  constructor(props){
+class About extends React.Component {
+  constructor(props) {
     super(props);
     console.log("About-constructor");
   }
 
-  componentDidMount(){
+  componentDidMount() {
     console.log("parent-About ComponentDidMount call");
   }
-  
 
-  render(){
+  render() {
     console.log("About-render");
 
-    return(
-         <>
-      <h1>Your About Us Page</h1>
-     
-      {/* <Link to={"/profile"}>Click me <Profile/></Link> */}
-      {/* <Outlet/> */}
+    return (
+      <>
+        <h1>Your About Us Page</h1>
 
-      <div style={{
-        marginTop:"30px",
-        marginBottom: "30px"
-      }}>
-        <h2 style={{color: "darkgreen"}}>See the difference between the Class Component and the Function Component</h2>
+        {/* <Link to={"/profile"}>Click me <Profile/></Link> */}
+        {/* <Outlet/> */}
 
-      </div>
+        <div
+          style={{
+            marginTop: "30px",
+            marginBottom: "30px",
+          }}
+        >
+          <h2 style={{ color: "darkgreen" }}>
+            See the difference between the Class Component and the Function
+            Component
+          </h2>
+        </div>
 
-     
         <Profile name={"AkashyFunctional"} />
         <ProfileClassComponent name={"AkashayClass"} XYZ="abc" />
-      
-    </>
-    )
-
+      </>
+    );
   }
 }
 
-export default About
+export default About;
--- 5_Cheap_me_code/src/Component/NavPages/ProfileClass.jsx
+++ 5_Cheap_me_code/src/Component/NavPages/ProfileClass.jsx
@@ -5,17 +5,38 @@ class Profile extends React.Component {
     constructor(props){
         super(props);
         this.state={
-         count: 0,
-         count2: 0
+         userInfo: {
+          name : "name",
+          login: "",
+          avatar_url: "",
+
+         },
         
         };
         console.log("ProfileClass-constructor"); //1st render
     }
 
-    componentDidMount(){
+    async componentDidMount(){
       // Its old uses like useEffect but it is not exactly the UseEffect
       //This method is best place to make Api Call
       console.log("ComponentDidMount ProfileClass Render"); //3rd render
+
+      //API Calls
+      const data = await fetch("https://api.github.com/users/Vishal7410")
+      const json = await data.json();
+      console.log(json);
+      this.setState({
+        userInfo: json,
+      });
+      //console.log("Child - componentDidMount" + this.props.userInfo.name);
+    }
+
+    componentDidUpdate() {
+      console.log("Component Did Update");
+
+    }
+    componentWillUnmount(){
+      console.log("Component Will Unmont");
     }
 
  
@@ -29,12 +50,16 @@ class Profile extends React.Component {
             backgroundColor: "blueviolet",
           }}
         >
-          <h1>This is my class Component </h1>        
+          {/* <h1>This is my class Component </h1>        
           <h2>props is Name : {this.props.name}</h2>
           <h2>Count: {this.state.count}</h2>
-          <h2>Count: {this.state.count2}</h2>
+          <h2>Count: {this.state.count2}</h2> */}
+           <h1>This is my class Component </h1>        
+          <h2>GitHub Name : {this.state.userInfo.name}</h2>
+          <h2>GitHub userName: {this.state.userInfo.login}</h2>
+         <img src={this.state.userInfo.avatar_url} alt="" />
 
-          <button 
+          {/* <button 
           onClick={()=> {
             //WE DO NOT MUTATED STATE DIRECTLY
             // NEVER DO this.state = someThing
@@ -43,7 +68,7 @@ class Profile extends React.Component {
                 count2: 2,
             });
           }}
-          >Click me TOO</button>
+          >Click me TOO</button> */}
         </div>
       </>
     );
